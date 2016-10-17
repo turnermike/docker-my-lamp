c186d871f006928866cd96848946c4ccb6722bd5
Mike Turner <mturner@realinteractive.ca>
2016-10-17 12:28:24 -0400

    testing deploy

--- deploy/db.php
+++ deploy/db.php
@@ -8,23 +8,24 @@ error_reporting(-1);
    PDO
    ========================================================================== */
 
-// // $dsn = 'mysql:dbname=test_php_app;host=db';
-// // $user = 'test_app_user';
-// // $password = 'test_app_password';
-// $dsn = 'mysql:dbname=steelhouse_website;host=steelhousedbcluster.cluster-cseokarizhz1.us-east-1.rds.amazonaws.com';
-// $user = 'realinteractive';
-// $password = 'O#43Sf%!aE987';
+// $dsn = 'mysql:dbname=test_php_app;host=db';
+// $user = 'test_app_user';
+// $password = 'test_app_password';
+$dsn = 'mysql:dbname=steelhouse_website;host=steelhousedbcluster.cluster-cseokarizhz1.us-east-1.rds.amazonaws.com';
+$user = 'realinteractive';
+$password = 'O#43Sf%!aE987';
 
-// try {
-//     $dbh = new PDO($dsn, $user, $password);
-// } catch (PDOException $e) {
-//     echo 'Connection failed: ' . $e->getMessage();
-// }
+try {
+    $dbh = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
 
-// foreach($dbh->query("Show variables like '%char%'") as $row) {
-//     printf("%s: %s<br />", htmlspecialchars($row[0]), htmlspecialchars($row[1]));
-// }
+foreach($dbh->query("Show variables like '%char%'") as $row) {
+    printf("%s: %s<br />", htmlspecialchars($row[0]), htmlspecialchars($row[1]));
+}
 
+echo '<br><br>testing abc123';
 
 /* ==========================================================================
    mysql_connect
@@ -32,25 +33,25 @@ error_reporting(-1);
 
 
 
-//$link = mysql_connect('localhost', 'root', 'root');
-$link = mysql_connect('steelhousedbcluster.cluster-cseokarizhz1.us-east-1.rds.amazonaws.com', 'realinteractive', 'O#43Sf%!aE987');
+// //$link = mysql_connect('localhost', 'root', 'root');
+// $link = mysql_connect('steelhousedbcluster.cluster-cseokarizhz1.us-east-1.rds.amazonaws.com', 'realinteractive', 'O#43Sf%!aE987');
 
-if (!$link) {
-    die('<br />Could not connect: ' . mysql_error());
-}
-echo '<br />Connected successfully';
-
-
-$db_selected = mysql_select_db('steelhouse_website', $link);
-
-if(!$db_selected){
- die('<br />Could not use backbone_address_book: ' . mysql_error());
-}
-
-echo '<br />Selected DB Successfully';
+// if (!$link) {
+//     die('<br />Could not connect: ' . mysql_error());
+// }
+// echo '<br />Connected successfully';
 
 
-mysql_close($link);
+// $db_selected = mysql_select_db('steelhouse_website', $link);
+
+// if(!$db_selected){
+//  die('<br />Could not use backbone_address_book: ' . mysql_error());
+// }
+
+// echo '<br />Selected DB Successfully';
+
+
+// mysql_close($link);
 
 
 
