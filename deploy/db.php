b51185531d8ab0105dd35bd677b99fe388616506
Mike Turner <mturner@realinteractive.ca>
2016-10-17 11:58:58 -0400

    added mysql_connect to db.php

--- deploy/db.php
+++ deploy/db.php
@@ -1,22 +1,58 @@
 <?php
-// $dsn = 'mysql:dbname=test_php_app;host=db';
-// $user = 'test_app_user';
-// $password = 'test_app_password';
 
-$dsn = 'mysql:dbname=steelhouse_website;host=steelhousedbcluster.cluster-cseokarizhz1.us-east-1.rds.amazonaws.com';
-$user = 'realinteractive';
-$password = 'O#43Sf%!aE987';
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
+/* ==========================================================================
+   PDO
+   ========================================================================== */
+
+// // $dsn = 'mysql:dbname=test_php_app;host=db';
+// // $user = 'test_app_user';
+// // $password = 'test_app_password';
+// $dsn = 'mysql:dbname=steelhouse_website;host=steelhousedbcluster.cluster-cseokarizhz1.us-east-1.rds.amazonaws.com';
+// $user = 'realinteractive';
+// $password = 'O#43Sf%!aE987';
+
+// try {
+//     $dbh = new PDO($dsn, $user, $password);
+// } catch (PDOException $e) {
+//     echo 'Connection failed: ' . $e->getMessage();
+// }
+
+// foreach($dbh->query("Show variables like '%char%'") as $row) {
+//     printf("%s: %s<br />", htmlspecialchars($row[0]), htmlspecialchars($row[1]));
+// }
+
+
+/* ==========================================================================
+   mysql_connect
+   ========================================================================== */
 
 
 
-try {
-    $dbh = new PDO($dsn, $user, $password);
-} catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+//$link = mysql_connect('localhost', 'root', 'root');
+$link = mysql_connect('steelhousedbcluster.cluster-cseokarizhz1.us-east-1.rds.amazonaws.com', 'realinteractive', 'O#43Sf%!aE987');
+
+if (!$link) {
+    die('<br />Could not connect: ' . mysql_error());
+}
+echo '<br />Connected successfully';
+
+
+$db_selected = mysql_select_db('steelhouse_website', $link);
+
+if(!$db_selected){
+ die('<br />Could not use backbone_address_book: ' . mysql_error());
 }
 
-foreach($dbh->query("Show variables like '%char%'") as $row) {
-    printf("%s: %s<br />", htmlspecialchars($row[0]), htmlspecialchars($row[1]));
-}
+echo '<br />Selected DB Successfully';
 
-echo '<br />testing 123 456';
+
+mysql_close($link);
+
+
+
+
+
